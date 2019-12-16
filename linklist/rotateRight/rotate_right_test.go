e054499787ad8d94316d20435ea2956b98a5af10
zhaoqi <zhao361020136@gmail.com>
2019-12-16 18:14:57 +0800

    first

--- linklist/rotateRight/rotate_right_test.go
+++ linklist/rotateRight/rotate_right_test.go
@@ -1,12 +1,61 @@
 package rotateRight
 
+/**
+给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+示例 1:
+输入: 1->2->3->4->5->NULL, k = 2
+输出: 4->5->1->2->3->NULL
+解释:
+向右旋转 1 步: 5->1->2->3->4->NULL
+向右旋转 2 步: 4->5->1->2->3->NULL
+示例 2:
+
+输入: 0->1->2->NULL, k = 4
+输出: 2->0->1->NULL
+解释:
+向右旋转 1 步: 2->0->1->NULL
+向右旋转 2 步: 1->2->0->NULL
+向右旋转 3 步: 0->1->2->NULL
+向右旋转 4 步: 2->0->1->NULL
+*/
 import (
 	"algo/linklist/singleLinkedListNoHead"
 	"testing"
 )
 
 func rotateRight(head *singleLinkedListNoHead.ListNode, k int) *singleLinkedListNoHead.ListNode {
+	phead := &singleLinkedListNoHead.ListNode{
+		Val:  0,
+		Next: head,
+	}
 
+	if head == nil {
+		return nil
+	} else if head.Next == nil {
+		return head
+	}
+
+	i := 1
+	for i <= k {
+		start := phead
+		node := phead.Next
+		preNode := node
+		prePreNode := node
+
+		//移动node到末尾
+		for ; node != nil; node = node.Next {
+			prePreNode = preNode
+			preNode = node
+		}
+
+		prePreNode.Next = preNode.Next
+		preNode.Next = start.Next
+		start.Next = preNode
+
+		i++
+	}
+
+	return phead.Next
 }
 
 func run(a []int, k int) {
@@ -18,7 +67,7 @@ func run(a []int, k int) {
 }
 
 func TestFirst(t *testing.T) {
-	a := []int{1, 2, 3, 4}
+	a := []int{0, 1, 2}
 	k := 4
 	run(a, k)
 }
