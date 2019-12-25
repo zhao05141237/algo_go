605db2a914249e860b148b3425bffd376e8ecbb7
zhaoqi <zhao361020136@gmail.com>
2019-12-25 18:19:46 +0800

    使用最小堆 合并有序数组

--- tree/heap/heap.go
+++ tree/heap/heap.go
@@ -2,6 +2,11 @@ package heap
 
 import "fmt"
 
+const (
+	MaxTopSort = "asc"
+	MinTopSort = "desc"
+)
+
 type Heap struct {
 	a     []int
 	n     int
@@ -19,7 +24,7 @@ func NewHeap(n int) *Heap {
 }
 
 //插入一个元素
-func (h *Heap) InsertData(data int) {
+func (h *Heap) InsertData(data int, sort string) {
 	//堆满
 	if h.count == h.n {
 		return
@@ -30,8 +35,10 @@ func (h *Heap) InsertData(data int) {
 	parent := h.count / 2
 	i := h.count
 
+	op := getOpBySort(sort)
+
 	for parent > 0 {
-		if h.a[parent] < h.a[i] {
+		if compare(h.a[parent], h.a[i], op) {
 			//子节点大于父节点 则交换两个节点
 			h.a[parent], h.a[i] = h.a[i], h.a[parent]
 			i = parent
@@ -42,14 +49,14 @@ func (h *Heap) InsertData(data int) {
 	}
 }
 
-func (h *Heap) RemoveMaxData() {
+func (h *Heap) RemoveTopData(sort string) {
 	//交换根结点和最后一个节点
 	if h.count == 0 {
 		return
 	}
 	if h.count == 1 {
-		//只有一个节点 删除顶结点
-		h.a[h.count] = 0
+		//只有一个节点 删除顶结点 此处是假删除
+		//h.a[h.count] = 0
 		h.count--
 		return
 	}
@@ -57,25 +64,49 @@ func (h *Heap) RemoveMaxData() {
 	h.a[1], h.a[h.count] = h.a[h.count], h.a[1]
 	h.count--
 	//从顶至下最大化
-	heapingUptoDown(h.a, h.count)
+	heapingUptoDown(h.a, h.count, sort)
 }
 
-func heapingUptoDown(a []int, count int) {
+func heapingUptoDown(a []int, count int, sort string) {
 	parent := 1
 	leftChild := 2 * parent
 	RightChild := leftChild + 1
+	op := getOpBySort(sort)
 
-	for RightChild <= count && (a[parent] < a[leftChild] || a[parent] < a[RightChild]) {
-		if a[parent] < a[leftChild] {
+	for RightChild <= count && (compare(a[parent], a[leftChild], op) || compare(a[parent], a[RightChild], op)) {
+		if compare(a[parent], a[leftChild], op) {
 			a[parent], a[leftChild] = a[leftChild], a[parent]
 		}
-		if a[parent] < a[RightChild] {
+		if compare(a[parent], a[RightChild], op) {
 			a[parent], a[RightChild] = a[RightChild], a[parent]
 		}
 		parent = leftChild
 		leftChild = 2 * parent
 		RightChild = leftChild + 1
 	}
+}
+
+func compare(left int, right int, op string) bool {
+	switch op {
+	case ">":
+		return left > right
+	case "<":
+		return left < right
+	case ">=":
+		return left >= right
+	case "<=":
+		return left <= right
+	default:
+		return false
+	}
+}
+
+func getOpBySort(sort string) string {
+	op := "<"
+	if sort == MinTopSort {
+		op = ">"
+	}
+	return op
 }
 
 func (h *Heap) printHeap() {
--- tree/heap/heap_action_test.go
+++ tree/heap/heap_action_test.go
@@ -7,19 +7,66 @@ import (
 )
 
 func TestHeap_InsertData(t *testing.T) {
+	//堆排序简单版本
 	rand.Seed(time.Now().UnixNano())
 	h := NewHeap(10)
+	//建最大堆
 	for i := 1; i <= 10; i++ {
-		h.InsertData(rand.Intn(100))
+		h.InsertData(rand.Intn(100), MaxTopSort)
 	}
-	//a := []int{82,79,77,24,25,3,51,14,12,2}
-	//for _, value := range a {
-	//	h.InsertData(value)
-	//}
 	h.printHeap()
+	//每次删除顶元素 循环n-1次 从而形成从有序数组
 	for i := 1; i < 10; i++ {
-		h.RemoveMaxData()
+		h.RemoveTopData(MaxTopSort)
 	}
 	h.printHeap()
+}
 
+func TestMergedSortedData(t *testing.T) {
+	//合并有序数组
+	const MaxSize = 4
+	a := [MaxSize][]int{
+		{9, 10, 12, 30, 32, 32, 39, 61, 77, 87},
+		{23, 27, 30, 63, 73, 72, 67, 79, 95, 97},
+		{1, 5, 6, 18, 24, 56, 62, 65, 88, 92},
+		{7, 15, 30, 31, 71, 56, 41, 43, 72, 97},
+	}
+	h := NewHeap(MaxSize + 1)
+	b := make([]int, MaxSize)        //存放每个数组每次推进的位置
+	countRow := make([]int, MaxSize) //存放每个数组的长度
+	sumCountRow := 0
+	var c []int //存放最终有序数组
+	i := 0
+	row := 0
+	col := 0
+	//首先用每个有序数组的首元素建最小堆
+	for i < MaxSize {
+		countRow[i] = len(a[i])
+		sumCountRow += countRow[i]
+		h.InsertData(a[i][0], MinTopSort)
+		i++
+	}
+	//每次取堆顶元素放置最终排序数组
+	for j := 0; j < sumCountRow; j++ {
+		h.RemoveTopData(MinTopSort)   //取堆顶元素
+		c = append(c, h.a[h.count+1]) //堆顶元素追加到最终数组
+		for i = 0; i < MaxSize; i++ {
+			//确定推进哪个有序数组
+			if b[i] < countRow[i] && a[i][b[i]] == h.a[h.count+1] {
+				b[i]++
+				row = i
+				col = b[i]
+				break
+			}
+		}
+		//某个数组遍历结束 则继续取堆顶元素
+		if col >= countRow[i] {
+			continue
+		}
+		//将数组添加到最小堆
+		h.InsertData(a[row][col], MinTopSort)
+	}
+	t.Log(c)
+	t.Log(len(c))
+	t.Log(cap(c))
 }
